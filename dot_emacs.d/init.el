e0904ef5a774cbe361a1c2018d2d3cdb048e945e
Samuel Husso <shusso@iki.fi>
2017-05-24 15:30:54 +0300

    golang support for emacs

--- dot_emacs.d/init.el
+++ dot_emacs.d/init.el
@@ -116,6 +116,34 @@
 (defun switch-to-previous-buffer ()
   (interactive)
   (switch-to-buffer (other-buffer (current-buffer) 1)))
+
+
+;; GOLANG ;;
+(defun auto-complete-for-go ()
+  (auto-complete-mode 1))
+
+(defun my-go-mode-hook ()  
+  ; Call Gofmt before saving
+  ; (add-hook 'before-save-hook 'gofmt-before-save)
+  (setq gofmt-command "goimports")                ; gofmt uses invokes goimports
+  (if (not (string-match "go" compile-command))
+      (set (make-local-variable 'compile-command)
+           "go build -v && go test -v && go vet"))
+  ;; guru settings
+  (go-guru-hl-identifier-mode)                    ; highlight identifiers
+  
+  ;; Key bindings specific to go-mode
+  (local-set-key (kbd "M-.") 'godef-jump)         ; Go to definition
+  (local-set-key (kbd "M-*") 'pop-tag-mark)       ; Return from whence you came
+  (local-set-key (kbd "M-p") 'compile)            ; Invoke compiler
+  (local-set-key (kbd "M-P") 'recompile)          ; Redo most recent compile cmd
+  (local-set-key (kbd "M-\\") 'next-error)         ; Go to next error (or msg)
+  (local-set-key (kbd "M-\'") 'previous-error)     ; Go to previous error or msg
+  ;; Misc go stuff
+  (auto-complete-mode 1))
+
+
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;           FUNCTION end           ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -289,6 +317,12 @@
 (require 'projectile)
 (setq-default projectile-mode t)
 
+(require 'go-mode)
+(require 'go-autocomplete)
+(require 'go-guru)
+
+(require 'neotree)
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;            MODES end             ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -333,8 +367,10 @@
 
 (setq initial-scratch-message nil)
 
-;;(tool-bar-mode nil)
-;;(menu-bar-mode nil)
+
+(tool-bar-mode nil)
+;;(scroll-bar-mode nil)
+(menu-bar-mode nil)
 
 ;; Visual feedback on selections
 (setq-default transient-mark-mode t)
@@ -374,6 +410,18 @@
 
 ;; 24.4. wrap lines
 (global-visual-line-mode 1)
+
+;; hook for golang
+(add-hook 'go-mode-hook 'my-go-mode-hook)
+;; hook for golang compile
+(add-hook 'go-mode-hook 'my-go-mode-hook)
+
+(add-hook 'go-mode-hook 'auto-complete-for-go)
+
+(with-eval-after-load 'go-mode
+   (require 'go-autocomplete))
+
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;           CUSTOMS end            ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -446,6 +494,11 @@
 (global-set-key "\M-s" 'tags-search)
 
 
+
+;; neotree
+(global-set-key (kbd "C-c d") 'neotree-toggle)
+(global-set-key (kbd "C-c j") 'neotree-dir)
+
 ;;
 ;; find-tags M-.
 ;;             list: tab or shift-/
@@ -512,7 +565,7 @@
  '(js-indent-level 4)
  '(package-selected-packages
    (quote
-    (go-projectile projectile go-mode jedi slime rainbow-delimiters auto-complete))))
+    (neotree go-complete go-autocomplete go-projectile projectile go-mode jedi slime rainbow-delimiters auto-complete))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
